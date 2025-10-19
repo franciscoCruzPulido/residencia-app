e5c6da9bebf4dcf7c9a524fde6d5fb432c0d08d3
Francisco <yuridiahernaandezflores@gmail.com>
2025-10-18 19:58:44 -0600

    Create contacto.jsx

---
+++ src/paginas/contacto.jsx
@@ -0,0 +1,10 @@
+import FormularioContacto from "../componentes/Formulariocontacto";
+
+export default function Contacto() {
+  return (
+    <div>
+      <h2>Contacto con la administración</h2>
+      <FormularioContacto />
+    </div>
+  );
+}
