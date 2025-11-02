d7e7c2577d1cb1403bfe7427c0fa5184ca2a6411
Francisco <yuridiahernaandezflores@gmail.com>
2025-11-01 20:17:11 -0600

    Update Approutes.jsx

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,27 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../paginas/login";
 import Dashboard from "../paginas/dashboard";
 import Avisos from "../paginas/avisos";
 import Visitas from "../paginas/visitas";
 import Residentes from "../paginas/residentes";
-import Contacto from "../paginascontacto";
-
+import Reportes from "../paginas/reportes";
+import Contacto from "../paginas/contacto";
+import Reservas from "../paginas/reservas";
 
 export default function Approutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/avisos" element={<Avisos />} />
-        <Route path="/visitas" element={<Visitas />} />
-        <Route path="/residentes" element={<Residentes />} />
-        <Route path="/contacto" element={<Contacto />} />
-
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/avisos" element={<Avisos />} />
+      <Route path="/visitas" element={<Visitas />} />
+      <Route path="/residentes" element={<Residentes />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/reportes" element={<Reportes />} />
+      <Route path="/reservas" element={<Reservas />} />
+    </Routes>
   );
 }
-
-
