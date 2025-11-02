6b5cc033eabd8f18e8eebae158df4611056e7a0c
Francisco <yuridiahernaandezflores@gmail.com>
2025-11-01 20:15:00 -0600

    Create reportes.jsx

---
+++ src/paginas/reportes.jsx
@@ -0,0 +1,10 @@
+import FormularioReporte from "../componentes/FormularioReporte";
+
+export default function Reportes() {
+  return (
+    <div>
+      <h1>Reportes de residentes</h1>
+      <FormularioReporte />
+    </div>
+  );
+}
