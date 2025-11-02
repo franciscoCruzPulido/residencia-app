ca4b2a646f08841c61859297ca1d6c7f002592ab
Francisco <yuridiahernaandezflores@gmail.com>
2025-11-01 20:15:29 -0600

    Create reservas.jsx

---
+++ src/paginas/reservas.jsx
@@ -0,0 +1,10 @@
+import FormularioReserva from "../componentes/FormularioReserva";
+
+export default function Reservas() {
+  return (
+    <div>
+      <h1>Reservas de residentes</h1>
+      <FormularioReserva />
+    </div>
+  );
+}
