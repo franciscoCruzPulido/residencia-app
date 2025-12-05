4f304b408ebcb5e7ad24364acde72ca95d989613
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:51:34 -0600

    Update visitas.jsx

--- src/sprint3/visitas.jsx
+++ src/sprint3/visitas.jsx
@@ -1,1 +1,10 @@
+import FormularioVisitas from "./componentes/formularioVisitas";
 
+export default function Visitas() {
+  return (
+    <div>
+      <h2>Registro de visitas</h2>
+      <FormularioVisitas />
+    </div>
+  );
+}
