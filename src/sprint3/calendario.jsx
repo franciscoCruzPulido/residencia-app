e71257b4ab19d7cc052aa1bc0689444f927eb2bd
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:49:59 -0600

    Update calendario.jsx

--- src/sprint3/calendario.jsx
+++ src/sprint3/calendario.jsx
@@ -1,1 +1,10 @@
+import Calendario from "./componentes/Calendario";
 
+export default function CalendarioPage() {
+  return (
+    <div>
+      <h1>Calendario</h1>
+      <Calendario />
+    </div>
+  );
+}
