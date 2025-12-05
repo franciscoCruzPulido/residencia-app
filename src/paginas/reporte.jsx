3fe770c4fb7d96fec86dbd999505ced53b9e3a53
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:21:39 -0600

    Update reporte.jsx

--- src/paginas/reporte.jsx
+++ src/paginas/reporte.jsx
@@ -1,1 +1,9 @@
+export default function Reporte() {
+  return (
+    <div>
+      <h1>Reporte de modificaciones</h1>
+      <p>Describe los cambios realizados en los sprints.</p>
+    </div>
+  );
+}
 
