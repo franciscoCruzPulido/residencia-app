f78f19103320c2cd1638338d67282702d75fc9b4
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:22:55 -0600

    Update Approutes.jsx

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../paginas/login";
-import Dashboard from "../paginas/dashboard";
+import Login from "../sprint1/login";
+import Dashboard from "../sprint1/dashboard";
 import Avisos from "../paginas/avisos";
-import Visitas from "../paginas/visitas";
-import Residentes from "../paginas/residentes";
-import Reportes from "../paginas/reportes";
-import Contacto from "../paginas/contacto";
-import Reservas from "../paginas/reservas";
+import Visitas from "../sprint3/visitas";
+import Residentes from "../sprint3/residentes";
+import Reportes from "../sprint2/reportes";
+import Contacto from "../sprint2/contacto";
+import Reservas from "../sprint2/reservas";
+import Actividades from "../paginas/actividades";
+import Sistema from "../paginas/sistema";
+import Reporte from "../paginas/reporte";
+import CalendarioPage from "../sprint3/calendario";
 
 export default function Approutes() {
   return (
@@ -20,6 +24,10 @@ export default function Approutes() {
       <Route path="/contacto" element={<Contacto />} />
       <Route path="/reportes" element={<Reportes />} />
       <Route path="/reservas" element={<Reservas />} />
+      <Route path="/actividades" element={<Actividades />} />
+      <Route path="/sistema" element={<Sistema />} />
+      <Route path="/reporte" element={<Reporte />} />
+      <Route path="/calendario" element={<CalendarioPage />} />
     </Routes>
   );
 }
