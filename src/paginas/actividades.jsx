8d30b0bbc3fc83ca62913bf00be2a4a7e4aba015
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:20:20 -0600

    Update actividades.jsx

--- src/paginas/actividades.jsx
+++ src/paginas/actividades.jsx
@@ -1,1 +1,11 @@
+export default function Actividades() {
+  return (
+    <div>
+      <h1>Actividades realizadas</h1>
+      <a href="/proyecto.pdf" download>
+        Descargar PDF de actividades
+      </a>
+    </div>
+  );
+}
 
