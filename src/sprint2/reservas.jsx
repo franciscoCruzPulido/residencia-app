9b38310c78407de3a93ca5472d8c8951edda6e67
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:32:55 -0600

    Update reservas.jsx

--- src/sprint2/reservas.jsx
+++ src/sprint2/reservas.jsx
@@ -1,1 +1,11 @@
+import FormularioReserva from "./componentes/FormularioReserva";
+
+export default function Reservas() {
+  return (
+    <div>
+      <h1>Reservas de residentes</h1>
+      <FormularioReserva />
+    </div>
+  );
+}
 
