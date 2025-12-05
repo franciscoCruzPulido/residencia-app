3f669c2e8a8345cab59f29c124ee693f3ed3f59d
Francisco <yuridiahernaandezflores@gmail.com>
2025-12-04 19:53:34 -0600

    Update tarjetaresidente.jsx

--- src/sprint3/componentes/tarjetaresidente.jsx
+++ src/sprint3/componentes/tarjetaresidente.jsx
@@ -1,1 +1,8 @@
-
+export default function Tarjetaresidente({ residente }) {
+  return (
+    <div style={{ border: "1px solid blue", margin: "10px", padding: "10px" }}>
+      <h3>{residente.nombre}</h3>
+      <p>Departamento: {residente.departamento}</p>
+    </div>
+  );
+}
